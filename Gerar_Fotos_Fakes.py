a64571e185599195d0965c2dc9829d49d7af3045
allansantosbrasill <allan1717_@hotmail.com>
2024-04-28 13:21:13 -0300

    Atualizado

--- Gerar_Fotos_Fakes.py
+++ Gerar_Fotos_Fakes.py
@@ -7,8 +7,23 @@ image_url = "https://thispersondoesnotexist.com/"
 # Especificando o caminho completo do diretório de destino
 pasta_destino = r"C:\Users\allan\OneDrive\Imagens\fotos fakes"
 
+while True:
+    fim = input("Digite quantas fotos você vai querer: ")
+    if fim.strip() == "":
+        print("Por favor, digite um número.")
+    elif not fim.isdigit():
+        print("Por favor, digite apenas números.")
+    else:
+        break
+
+quantidade_fotos = int(fim)
+
+print("Iniciando bot")
+print(f"Baixando {quantidade_fotos} aguarde...")
+
 # Variável de contagem para o nome do arquivo
 contador = 1
+contador_foto = 1
 
 while True:
     # Criando o nome do arquivo com o número atual
@@ -24,9 +39,12 @@ while True:
     response = requests.get(image_url)
 
     with open(caminho_completo, "wb") as f:
-        f.write(response.content)
+        f.write(response.content)   
     
-
     # Incrementando o contador para o próximo número
-    contador += 1
+    contador_foto += 1
+
+    if contador_foto > quantidade_fotos:
+        print(f"Foram baixadas {quantidade_fotos} fotos.")
+        break
 
